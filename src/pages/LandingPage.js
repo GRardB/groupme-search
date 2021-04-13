bbfb9f50880f81f0ee944c0fabe702fade5916bd
Gerard O'Neill <grardb@gmail.com>
2021-04-12 18:55:41 -0700

    Get GroupMe client ID from .env file

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -17,7 +17,7 @@ export const LandingPage = () => (
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
                 <a
-                  href="https://oauth.groupme.com/oauth/authorize?client_id=U9pXqhJ4sLfQXTPlaJ5lFGF7K111uExUXI17tkllXNFVa6MV"
+                  href={`https://oauth.groupme.com/oauth/authorize?client_id=${process.env.REACT_APP_GROUPME_CLIENT_ID}`}
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                 >
                   Get started
